0a9181140ce96fcb2102154644001377b0b169e4
Thinh Dinh <thinh.dinh@bisnode.com>
2019-02-07 18:29:58 +0200

    Created a Spring Boot project with libraries DevTools, Web, MySQL, FreeMarker, JPA

---
+++ src/main/java/com/herokuuapp/demo/springbatch/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.herokuuapp.demo.springbatch;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbatchDemoApplication.class);
+	}
+
+}
+
